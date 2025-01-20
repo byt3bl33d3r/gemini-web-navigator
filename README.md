29ec97781fd7e66f813ec6dabcd1a705439833c2
byt3bl33d3r <byt3bl33d3r@pm.me>
2025-01-20 00:08:07 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,87 @@
-# gemini-cloudflare-bypass
-Bypass Cloudflare turnstile using Google Gemini's object detection capabilities
+# Gemini Web Navigator Experiments
+
+This repo contains Proof-of-Concept code, experiments & notes about using Google Gemini's Vision capabilities and [Computer Use](https://docs.anthropic.com/en/docs/build-with-claude/computer-use)-style desktop manipulation for LLM enabled browser control.
+
+## Background
+
+One of the most unique features of [Google Gemini](https://gemini.google.com/?hl=en-GB) is it's ability to [return bounding box coordinates](https://ai.google.dev/gemini-api/docs/vision?lang=python#bbox) on objects in images (see [this blog post](https://simonwillison.net/2024/Aug/26/gemini-bounding-box-visualization/) for a nice overview).
+
+This got me thinking: how does it perform when asked to return bounding box coordinates for web page elements? Turns out it works suprisingly well on simple/medium complexity web pages.
+
+As this is a *PURELY* vision based LLM browser control system, you have the ability to select web page elements to interact with through screenshots using natural language but it also gives you a lot of the same cabilities of other projects like [browser-use]() or [skyvern](https://github.com/Skyvern-AI/skyvern) "on the cheap": no injecting custom JS into the browser context to highlight interactive elements on the webpage or complicated Agentic systems.
+
+Just to be clear I'm not saying this is better than the other projects I mentioned above, I'm just saying it's cheaper: this is ~200 lines of Python, no agentic system necessary and works suprisingly well on simple/medium complexity webpages. Choose the best tool for your use case.
+
+## Bypassing non-captcha based anti-bot products
+
+An interesting consequence to using this approach is that you're able to bypass most non-captcha based anti-bot products out of the box like [Cloudflare Turnstile](https://www.cloudflare.com/application-services/products/turnstile/). It's probably also effective against the "press and hold left mouse button"-style bot shields as well.
+
+From what I can surmise, this works mainly because of the following:
+1. We're using [patchright](https://github.com/Kaliiiiiiiiii-Vinyzu/patchright-python)
+2. We're not starting the browser in headless mode (we have a containerized desktop environment)
+3. We're not injecting custom JS into browser context.
+4. We're not disabling the Chrome sandbox (we're passing a `seccomp` profile to the docker container to start Chromium with all security features enabled)
+5. Mouse/Keyboard inputs are done through the desktop using a "Computer Use" style system, not using browser events (e.g. `mouseup`, `mousedown` etc..)
+
+Below is a video demonstration of this approach: the LLM bypasses Cloudflare Turnstyle and automatically navigates through a website to perform a search:
+
+[![Demo Video](https://img.youtube.com/vi/JO8jMHpOW90/0.jpg)](https://www.youtube.com/watch?v=JO8jMHpOW90)
+
+The "secret sauce" (if you can call it that) to bypass Turnstile are the following lines in the workflow YAML file we're passing on the command line ([full workflow file here]()):
+
+```yaml
+actions:
+
+  # This bypasses Cloudflare Turnstile
+  - element: checkbox to the left of "Verify you are human"
+    do:
+      - click:
+
+```
+
+This is just asking Gemini to return the bounding box coordinates in the screenshot it takes under the hood for the `checkbox to the left of "Verify you are human"` and then clicking on the returned coordinates through the desktop 💀.
+
+## Installation
+
+Easiest way to play around with this:
+
+1. Install Docker Desktop
+2. Clone this repo and open in VScode
+3. Install the Dev Containers extension
+4. Click "Re-open in devcontainer" when prompted or through the command pallete.
+5. Create a `.env` file or export your `GEMINI_API_KEY=`.
+
+Once everything is started, you'll have a `noVnc` session at `localhost:6081` that you can use to interact with the containerized desktop env and to watch the LLM control the browser.
+
+It's obviously possible to make a standalone `Dockefile` for this it's just a lot easier for my workflow this way. (PRs welcome!)
+
+## Usage
+
+You just need to give the path to a workflow YAML file to `gemini_navigator.py`. Workflow files are pretty self explanitory. Below is an example:
+
+```yaml
+url: https://securitytrails.com # Starting URL
+
+config:
+  interaction_pause: 3 # delay between each action
+  wait_until: networkidle # Playwright .goto() wait_until argument
+
+actions:
+
+  # This bypasses Cloudflare Turnstile
+  - element: checkbox to the left of "Verify you are human"
+    do:
+      - click:
+
+  - element: main search bar # You can select any element to interact with using natural language
+    do:
+      - click:
+      - type: example.com
+
+  - element: search button
+    do:
+      - click:
+      - screenshot:
+```
+
+They'll be a `pic.png` file created on each action that will have the screenshot with a drawn bounding box represting the coordinates Gemini returned.
---
+++ gemini_navigator.py
@@ -0,0 +1,210 @@
+import asyncio
+import argparse
+import io
+import json
+import sys
+import base64
+import google.generativeai as genai
+import shutil
+import shlex
+import itertools
+import yaml
+import tempfile
+import tkinter as tk
+from yaml import Loader
+from uuid import uuid4
+from pathlib import Path
+from collections import namedtuple
+from pydantic import SecretStr
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from patchright.async_api import async_playwright
+from PIL import Image, ImageDraw
+from utils import run
+
+OUTPUT_DIR = '/tmp/outputs'
+
+Coordinates = namedtuple("Coordinates", ["ymin", "xmin", "ymax", "xmax"])
+
+class ComputerUseError(Exception):
+    pass
+
+class Settings(BaseSettings):
+    model_config: SettingsConfigDict = SettingsConfigDict(env_file='.env', extra='ignore')
+    gemini_api_key: SecretStr
+
+class ComputerUse:
+    _SCREENSHOT_DELAY = 2.0
+    _TYPING_DELAY_MS = 12
+    _TYPING_GROUP_SIZE = 50
+
+    async def key(self, text: str):
+        return await self.shell(f"xdotool key -- {text}")
+
+    async def type(self, text: str, delay: int | None  = None):
+        for chunk in itertools.batched(text, n=self._TYPING_GROUP_SIZE):
+            await self.shell(
+                f"xdotool type --delay {delay or self._TYPING_DELAY_MS} -- {shlex.quote(''.join(chunk))}",
+                take_screenshot=False
+            )
+
+        return (await self.screenshot())[2]
+
+    async def left_click(self):
+        return await self.shell("xdotool click 1")
+
+    async def mouse_move(self, x: int, y: int):
+        return await self.shell(f"xdotool mousemove --sync {x} {y}")
+
+    async def screenshot(self):
+        """Take a screenshot of the current screen and return the base64 encoded image."""
+        output_dir = Path(OUTPUT_DIR)
+        output_dir.mkdir(parents=True, exist_ok=True)
+        path = output_dir / f"screenshot_{uuid4().hex}.png"
+
+        if not shutil.which("gnome-screenshot"):
+            raise ComputerUseError("Unable to find gnome-screenshot")
+
+        screenshot_cmd = f"gnome-screenshot -f {path} -p"            
+
+        stdout,stderr,_ = await self.shell(screenshot_cmd, take_screenshot=False)
+        if path.exists():
+            return stdout, stderr, base64.b64encode(path.read_bytes()).decode()
+
+        raise ComputerUseError(f"Failed to take screenshot: {stderr}")
+
+    async def shell(self, command: str, take_screenshot=True) -> tuple:
+        """Run a shell command and return the output, error, and optionally a screenshot."""
+        _, stdout, stderr = await run(command)
+        base64_image = None
+
+        if take_screenshot:
+            # delay to let things settle before taking a screenshot
+            await asyncio.sleep(self._SCREENSHOT_DELAY)
+            base64_image = (await self.screenshot())[2]
+
+        return stdout, stderr, base64_image
+
+class GeminiVision:
+    def __init__(self, computer_use: ComputerUse):
+        self.computer_use = computer_use
+        self.screen_width, self.screen_height = self.get_screen_size()
+        self.model = genai.GenerativeModel(model_name='gemini-1.5-pro')
+
+    def get_screen_size(self) -> tuple[int,int]:
+        # Create a tkinter root window
+        root = tk.Tk()
+
+        # Get screen dimensions
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+
+        root.destroy()
+
+        return screen_width, screen_height
+
+    async def get_bounding_box_center(self, element_description: str):
+
+        ymin, xmin, ymax, xmax = await self.get_bounding_box(element_description)
+
+        center_x = (xmin + xmax) / 2
+        center_y = (ymin + ymax) / 2
+
+        print(center_x, center_y)
+
+        return center_x, center_y
+
+    async def get_bounding_box(self, element_description: str) -> tuple:
+        prompt = f"Return a bounding box for the {element_description} in [ymin, xmin, ymax, xmax] format."
+
+        screenshot = base64.b64decode(
+            (await self.computer_use.screenshot())[2]
+        )
+        screenshot = Image.open(
+            io.BytesIO(screenshot)
+        )
+
+        while True:
+            try:
+                response = json.loads(
+                    (await self.model.generate_content_async([screenshot, prompt])).text
+                )
+                break
+            except json.JSONDecodeError:
+                print("json decode error")
+
+        print(response)
+        box = Coordinates(*response)
+
+        draw = ImageDraw.Draw(screenshot)
+        ymin = (box.ymin / 1000) * self.screen_height
+        ymax = (box.ymax / 1000) * self.screen_height
+        xmin = (box.xmin / 1000) * self.screen_width
+        xmax = (box.xmax / 1000) * self.screen_width
+
+        draw.rectangle([xmin, ymin, xmax, ymax], outline="red", width=2)
+        screenshot.save('pic.png')
+
+        return ymin, xmin, ymax, xmax
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('workflow_yaml_file', type=Path, metavar="workflow_yaml_file")
+    args = parser.parse_args()
+    
+    if not args.workflow_yaml_file.exists():
+        print('path to workflow yaml file does not exist!')
+        sys.exit(1)
+
+    settings = Settings()
+    genai.configure(api_key=settings.gemini_api_key.get_secret_value())
+
+    async def main():
+        computer_use = ComputerUse()
+        gemini_vision = GeminiVision(computer_use)
+
+        with tempfile.TemporaryDirectory() as tmpdir:
+            async with async_playwright() as p:
+                context = await p.chromium.launch_persistent_context(
+                    user_data_dir=tmpdir,
+                    channel='chromium',
+                    headless=False,
+                    no_viewport=True,
+                    # args=[
+                    #     '--no-first-run',
+                    #     '--no-default-browser-check',
+                    #     '--no-startup-window',
+                    #     '--window-position=0,0',
+                    # ]
+                )
+
+                page = await context.new_page()
+
+                with args.workflow_yaml_file.open() as f:
+                    workflow: dict= yaml.load(f, Loader=Loader)
+                    url: dict = workflow['url']
+                    config: dict = workflow['config']
+
+                await page.goto(url=url, wait_until=config.get('wait_until', 'networkidle'))
+
+                for action in workflow['actions']:
+                    await asyncio.sleep(config['interaction_pause'])
+
+                    center_x, center_y = await gemini_vision.get_bounding_box_center(action['element'])
+
+                    for desktop_action in action['do']:
+                        for name,action_config in desktop_action.items():
+
+                            if name == 'click':
+                                await computer_use.mouse_move(x=center_x, y=center_y)
+                                await computer_use.left_click()
+
+                            if name == 'type':
+                                await computer_use.type(desktop_action.get('type'))
+
+                            if name == 'screenshot':
+                                await page.screenshot(path='final_screenshot.png')
+                            
+                            if name == 'markdownify':
+                                raise NotImplementedError
+
+    asyncio.run(main())
